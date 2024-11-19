b900d8482317e18020da01f21033602f33ce57f5
Yanis Geurts <yanis.geurts@student.uliege.be>
2024-11-19 14:53:27 +0100

    tg theo

--- shallow.c
+++ shallow.c
@@ -396,16 +396,12 @@ int main(int argc, char **argv)
   struct data h_u;
   struct data h_v;
   init_data(&h_interp, px, py, param.dx, param.dy, 0.);
-  init_data(&h_u, coord[0] == 0 ? px+1:px , py, param.dx, param.dy, 0.);
-  init_data(&h_v, px, coord[0] == 0 ? py + 1 : py, param.dx, param.dy, 0.);
+  init_data(&h_u, coords[0] == 0 ? px+1:px , py, param.dx, param.dy, 0.);
+  init_data(&h_v, px, coords[0] == 0 ? py + 1 : py, param.dx, param.dy, 0.);
 
 
  
-
   double start = GET_TIME();
-
-
-
   //ici j'ai modifié les indices de boucles, on saute le premier car on aura besoin de ce qui a été send
   //par contre si on est au process en 0, 0, là il faudra gérer ce cas
   
@@ -479,9 +475,11 @@ int main(int argc, char **argv)
   MPI_Wait(&request2, MPI_STATUS_IGNORE);
   MPI_Wait(&request3, MPI_STATUS_IGNORE);
   MPI_Wait(&request4, MPI_STATUS_IGNORE);
-  
-  
 
+  double* up_row_v;
+  double* left_col_u;
+  double* left_col_eta;  
+  double* up_row_eta; 
   // boucle temporelle
   for(int n = 0; n < nt; n++) 
   {
@@ -532,15 +530,12 @@ int main(int argc, char **argv)
       exit(0);
     }
 
-    // update eta
-
-
     // Exchanging the last col and row of u and v on the grid
     MPI_Request request5, request6, request7, request8;
     
     if(coords[0] != 0)
     {
-      double* left_col_u; 
+      
       MPI_Irecv(left_col_u, u.ny, MPI_DOUBLE, neighbors[LEFT], neighbors[LEFT], cart_comm, &request5);
     }
     if(coords[0] != dims[0] - 1)
@@ -559,13 +554,13 @@ int main(int argc, char **argv)
     }
     if(coords[1] != 0)
     {
-      double* up_row_v; 
+       
       MPI_Irecv(up_row_v, v.nx, MPI_DOUBLE, neighbors[UP], neighbors[UP], cart_comm, &request7);
     }
     if(coords[1] != dims[1] - 1)
     {
       //Send the last row of v
-      double *down_row_v = (double *)malloc(v.nx * sizeof(double));
+      double *down_row_v = (double *)malloc(v.nx * sizeof(double)); //Il faut regarder si on doit free des deux côotés de la comm ou juste d'un
 
       for(int i = 0; i < v.nx; i++)
       {
@@ -595,59 +590,55 @@ int main(int argc, char **argv)
       }
     }
     // wait for everything to be received
-    if(coord[0]!= 0)
+    if(coords[0]!= 0)
     MPI_Wait(&request5, MPI_STATUS_IGNORE);
-    if(coord[1]!=0)
+    if(coords[1]!=0)
     MPI_Wait(&request7, MPI_STATUS_IGNORE);
     //Handling the upper left 
     double hui1j = GET(&h_u, coords[0]==0?1:0, 0);
     double huij = coords[0]==0?GET(&h_u, 0, 0):left_col_u[0];
-    double hvij1 = GET(&h_v, 0, coords[1]==0?1:0)
+    double hvij1 = GET(&h_v, 0, coords[1]==0?1:0);
     double hvij = coords[1]==0?GET(&h_v, 0, 0):up_row_v[0];
     
     
-    double eta_ij = GET(&eta, i, j)
-      - param.dt / param.dx * (hui1j * GET(&u, coords[0]==0?i+1:i, j) - huij * GET(&u, coords[0]==0?i:i-1, j))
-      - param.dt / param.dy * (hvij1 * GET(&v, i,coords[0]==0?j+1:j) - hvij * GET(&v, i, coords[0]==0?j:j-1));
-    SET(&eta, i, j, eta_ij);
+    double eta_ij = GET(&eta, 0, 0)
+      - param.dt / param.dx * (hui1j * GET(&u, coords[0]==0?1:0, 0) - huij * GET(&u, 0, 0))
+      - param.dt / param.dy * (hvij1 * GET(&v, 0,coords[0]==0?1:0) - hvij * GET(&v, 0, 0));
+    SET(&eta, 0, 0, eta_ij);
     
     //Handling the first row along y
     for(int i = 1; i < eta.nx; i++){
       double hui1j = GET(&h_u, coords[0]==0?i+1:i, 0);
       double huij = GET(&h_u, coords[0]==0?i:i-1, 0);
-      double hvij1 = GET(&h_v, i, coords[1]==0?1:0)
+      double hvij1 = GET(&h_v, i, coords[1]==0?1:0);
       double hvij = coords[1]==0?GET(&h_v, i, 0):up_row_v[i];
       
       
-      double eta_ij = GET(&eta, i, j)
-        - param.dt / param.dx * (hui1j * GET(&u, coords[0]==0?i+1:i, j) - huij * GET(&u, coords[0]==0?i:i-1, j))
-        - param.dt / param.dy * (hvij1 * GET(&v, i,coords[0]==0?j+1:j) - hvij * GET(&v, i, coords[0]==0?j:j-1));
-      SET(&eta, i, j, eta_ij);
+      double eta_ij = GET(&eta, i, 0)
+        - param.dt / param.dx * (hui1j * GET(&u, coords[0]==0?i+1:i, 0) - huij * GET(&u, coords[0]==0?i:i-1, 0))
+        - param.dt / param.dy * (hvij1 * GET(&v, i,coords[0]==0?1:0) - hvij * GET(&v, i, 0));
+      SET(&eta, i, 0, eta_ij);
     }
 
     // Handling the first col along x
     for(int j = 1; j < eta.nx; j++){
       double hui1j = GET(&h_u, coords[0]==0?1:0, j);
       double huij = coords[0]==0?GET(&h_u, 0, j):left_col_u[j];
-      double hvij1 = GET(&h_v, 0, coords[1]==0?j+1:j)
+      double hvij1 = GET(&h_v, 0, coords[1]==0?j+1:j);
       double hvij = GET(&h_v, 0, coords[1]==0?j:j-1);
       
         
-      double eta_ij = GET(&eta, i, j)
-        - param.dt / param.dx * (hui1j * GET(&u, coords[0]==0?i+1:i, j) - huij * GET(&u, coords[0]==0?i:i-1, j))
-        - param.dt / param.dy * (hvij1 * GET(&v, i,coords[0]==0?j+1:j) - hvij * GET(&v, i, coords[0]==0?j:j-1));
-      SET(&eta, i, j, eta_ij);
+      double eta_ij = GET(&eta, 0, j)
+        - param.dt / param.dx * (hui1j * GET(&u, coords[0]==0?1:0, j) - huij * GET(&u, 0, j))
+        - param.dt / param.dy * (hvij1 * GET(&v, 0,coords[0]==0?j+1:j) - hvij * GET(&v, 0, coords[0]==0?j:j-1));
+      SET(&eta, 0, j, eta_ij);
     }
-    
-
-
-
     // Send eta
     MPI_Request request9, request10, request11, request12;
     
     if(coords[0] != 0)
     {
-      double* left_col_eta; 
+      
       MPI_Irecv(left_col_eta, eta.ny, MPI_DOUBLE, neighbors[LEFT], neighbors[LEFT], cart_comm, &request9);
       // Appeler la méthode qui le reçoit
     }
@@ -667,7 +658,7 @@ int main(int argc, char **argv)
     }
     if(coords[1] != 0)
     {
-      double* up_row_eta; 
+      
       MPI_Irecv(up_row_eta, eta.nx, MPI_DOUBLE, neighbors[UP], neighbors[UP], cart_comm, &request11);
     }
     if(coords[1] != dims[1] - 1)
@@ -684,32 +675,54 @@ int main(int argc, char **argv)
     }
     // update u and v
 
-    // il va falloir séparer en 2 pour le cas avec u et v puisqu'ils ne font pas la même taille. Ce sera plus simple
-
-    for(int i = 0; i < nx; i++) 
+    for(int i = 1; i < u.nx; i++) 
     {
-      for(int j = 0; j < ny; j++) 
+      for(int j = 1; j < v.ny; j++) 
       {
+        
         double c1 = param.dt * param.g;
         double c2 = param.dt * param.gamma;
         double eta_ij = GET(&eta, i, j);
-        double eta_imj = GET(&eta, (i == 0) ? 0 : i - 1, j);
-        double eta_ijm = GET(&eta, i, (j == 0) ? 0 : j - 1);
-        double u_ij = (1. - c2) * GET(&u, i, j)
-          - c1 / param.dx * (eta_ij - eta_imj);
-        double v_ij = (1. - c2) * GET(&v, i, j)
-          - c1 / param.dy * (eta_ij - eta_ijm);
-        SET(&u, i, j, u_ij);
-        SET(&v, i, j, v_ij);
+        
+        if(i < v.nx){
+          double eta_imj = GET(&eta, i - 1, j);
+          double u_ij = (1. - c2) * GET(&u, i, j)
+            - c1 / param.dx * (eta_ij - eta_imj);
+          SET(&u, i, j, u_ij);
+        }
+        if(j < u.ny){
+          double eta_ijm = GET(&eta, i, j - 1);
+          double v_ij = (1. - c2) * GET(&v, i, j)
+            - c1 / param.dy * (eta_ij - eta_ijm);
+          SET(&v, i, j, v_ij);
+        }
       }
     }
     //WAIT eta 
-    if(coord[0]!= 0)
-    MPI_Wait(&request9, MPI_STATUS_IGNORE);
-    if(coord[1]!=0)
-    MPI_Wait(&request11, MPI_STATUS_IGNORE);
-
-
+    if(coords[0]!= 0)
+      MPI_Wait(&request9, MPI_STATUS_IGNORE);
+    if(coords[1]!=0)
+      MPI_Wait(&request11, MPI_STATUS_IGNORE);
+    for(int j = 0; j < u.ny; j++) 
+    {
+      double c1 = param.dt * param.g;
+      double c2 = param.dt * param.gamma;
+      double eta_ij = GET(&eta, 0, j);
+      double eta_imj = coords[0]==0?GET(&eta, 0, j):left_col_eta[j];
+      double u_ij = (1. - c2) * GET(&u, 0, j)
+        - c1 / param.dx * (eta_ij - eta_imj);
+      SET(&u, 0, j, u_ij);
+    }
+    for(int i = 0; i < v.nx; i++) 
+    {
+      double c1 = param.dt * param.g;
+      double c2 = param.dt * param.gamma;
+      double eta_ij = GET(&eta, 0, i);
+      double eta_ijm = coords[1]==0?GET(&eta, 0, i):up_row_eta[i];
+      double v_ij = (1. - c2) * GET(&v, i, 0)
+        - c1 / param.dy * (eta_ij - eta_ijm);
+      SET(&v, i, 0, v_ij);
+    }
   }
 
   MPI_Finalize();
@@ -725,8 +738,10 @@ int main(int argc, char **argv)
   printf("\nDone: %g seconds (%g MUpdates/s)\n", time,
          1e-6 * (double)eta.nx * (double)eta.ny * (double)nt / time);
 
-  // Il faut free les h_u ...
+
   free_data(&h_interp);
+  free_data(&h_u);
+  free_data(&h_v);
   free_data(&eta);
   free_data(&u);
   free_data(&v);
